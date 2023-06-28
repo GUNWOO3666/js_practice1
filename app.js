5a90b1a3288377ef07d4ba3d509540e92289c774
GUNWOO3666 <gunwoo3666@gmail.com>
2023-06-28 17:26:21 +0900

    justPractice

---
+++ app.js
@@ -0,0 +1,23 @@
+const calculator = {
+    add: function(a, b){
+        console.log(a + b);
+    },
+    minus: function(a, b){
+        console.log(a - b);
+    },
+    times: function(a, b){
+        console.log(a * b);
+    },
+    divide: function(a, b){
+        console.log(a / b);
+    },
+    powerOf: function(a, b){
+        console.log(a ** b);
+    }
+}
+
+calculator.add(5, 7)
+calculator.minus(5, 7)
+calculator.times(5, 7)
+calculator.divide(5, 7)
+calculator.powerOf(5, 7)
